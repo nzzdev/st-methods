89fa1448f2c39ddc9843584a9ffd90db77761ef9
Manuel Roth <manuel.roth@nzz.ch>
2021-04-22 19:21:00 +0200

    Move helper functions to helpers.py

--- bots/de-corona/risklayer_icu/risklayer_icu.py
+++ bots/de-corona/risklayer_icu/risklayer_icu.py
@@ -1,70 +1,17 @@
-import os
-import pandas as pd
-import gspread
-import json
-from datetime import datetime, timedelta
 from time import sleep
-
-
-def download_sheet(sh, name):  # function for sheet download
-    try:
-        wsh = sh.worksheet(name)
-        return wsh
-    except gspread.exceptions.APIError as e:
-        if e.response.status_code == 429:
-            sleep(5)
-            try:
-                wsh = sh.worksheet(name)
-                return wsh
-            except gspread.exceptions.APIError as e:
-                print('Script failed twice (blacklisted?):', e)
-        elif e.response.status_code > 499:
-            sleep(20)
-            try:
-                wsh = sh.worksheet(name)
-                return wsh
-            except gspread.exceptions.APIError as e:
-                print('Script failed twice (check source):', e)
-        else:
-            print('Other HTTP error (not 429 or 500):', e)
-    except gspread.exceptions.APIError as e:
-        print('Other URL error:', e)
-
-
-def updateChart(id, title="", subtitle="", notes="", data=pd.DataFrame()):  # Q helper function
-    # read qConfig file
-    json_file = open('../q.config.json')
-    qConfig = json.load(json_file)
-
-    # update chart properties
-    for item in qConfig.get('items'):
-        for environment in item.get('environments'):
-            if environment.get('id') == id:
-                if title != '':
-                    item.get('item').update({'title': title})
-                if subtitle != '':
-                    item.get('item').update({'subtitle': subtitle})
-                if notes != '':
-                    item.get('item').update({'notes': notes})
-                if data.size > 0:
-                    # reset_index() and T (for transpose) are used to bring column names into the first row
-                    transformed_data = data.applymap(str).reset_index(
-                        drop=False).T.reset_index().T.apply(list, axis=1).to_list()
-                    if 'table' in item.get('item').get('data'):
-                        item.get('item').get('data').update(
-                            {'table': transformed_data})
-                    else:
-                        item.get('item').update({'data': transformed_data})
-                print('Successfully updated item with id', id,
-                      'on', environment.get('name'), 'environment')
-    # write qConfig file
-    with open('../q.config.json', 'w', encoding='utf-8') as json_file:
-        json.dump(qConfig, json_file, ensure_ascii=False, indent=1)
-    json_file.close()
+from datetime import datetime, timedelta
+import gspread
+import pandas as pd
+import os
+import sys
 
 
 if __name__ == '__main__':
     try:
+        # add parent directory to path so helpers file can be referenced
+        sys.path.append(os.path.dirname((os.path.dirname(__file__))))
+        from helpers import *
+
         # set working directory, change if necessary
         os.chdir(os.path.dirname(__file__))
 
@@ -144,8 +91,8 @@ if __name__ == '__main__':
             timestamp_str
 
         # insert id and subtitle manually and run function
-        updateChart(id='245e5a30acb9ffa8e53b336e6b83c7bc',
-                    data=df, notes=notes_chart)
+        update_chart(id='245e5a30acb9ffa8e53b336e6b83c7bc',
+                     data=df, notes=notes_chart)
 
     except:
         raise
