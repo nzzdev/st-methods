b6c0faabf11dbcdf8e930bc0cefc25641ef27019
Manuel Roth <manuel.roth@nzz.ch>
2021-04-27 17:29:34 +0200

    Add R script to Github Action and create helpers.R file to consolidate R helper functions

--- bots/corona-charts/owid_vaccinations_worldwide/owid_vaccinations_worldwide.R
+++ bots/corona-charts/owid_vaccinations_worldwide/owid_vaccinations_worldwide.R
@@ -7,38 +7,8 @@ library(tidyverse)
 library(clipr)
 library(reticulate)
 
-library(jsonlite)
-
-update_chart <- function(id, title = "", subtitle = "", notes = "", data = list()) {
-  qConfig <- fromJSON("../q.config.json", simplifyDataFrame = TRUE)
-  for (item in qConfig$items) {
-    index <- 0
-    for (environment in qConfig$items$environments) {
-      index <- index + 1
-      if (environment$id == id) {
-        if (title != "") {
-          qConfig$items$item$title[[index]] <- title
-        }
-        if (subtitle != "") {
-          qConfig$items$item$subtitle[[index]] <- subtitle
-        }
-        if (notes != "") {
-          qConfig$items$item$notes[[index]] <- notes
-        }
-        if (length(data) > 0) {
-          qConfig$items$item$data[[index]] <- rbind(names(data), as.matrix(data))
-        }
-        print(paste0("Successfully updated item with id ", id))
-      }
-    }
-  }
-  qConfig <- toJSON(qConfig, pretty = TRUE)
-  write(qConfig, "../q.config.json")
-}
-
-
-# set working directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# import helper functions
+source("../helpers.R")
 
 # read-in
 owid_raw <- read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
