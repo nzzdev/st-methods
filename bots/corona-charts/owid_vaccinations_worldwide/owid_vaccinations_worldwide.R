cf46c61d89dec03c86ade56a5d15b93e8c92b4e3
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2021-05-03 12:36:51 +0200

    Use unicode thin space instead of regular white space in dates

--- bots/corona-charts/owid_vaccinations_worldwide/owid_vaccinations_worldwide.R
+++ bots/corona-charts/owid_vaccinations_worldwide/owid_vaccinations_worldwide.R
@@ -112,6 +112,6 @@ vacc_final$iso_code[vacc_final$location == "EU"] <- "EU"
 vacc_final$people_fully_vaccinated[vacc_final$location == "Welt"] <- NA
 vacc_final$people_fully_vaccinated[vacc_final$location == "EU"] <- NA
 
-notes_string <- paste("Stand:", format(Sys.Date(), "%d. %m. %Y"), sep = " ")
+notes_string <- paste("Stand:", format(Sys.Date(), "%d. %m. %Y"), sep = " ")
 
 update_chart(id = "79af3c6593df15827ccb5268a7aff0be", data = vacc_final, notes = notes_string)
