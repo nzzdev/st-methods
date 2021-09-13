636e4f67031c91cb849b0dda89d1cfcf1433be29
Nikolai Thelitz <nikolai.thelitz@gmail.com>
2021-09-13 15:49:17 +0200

    Update corona-auto-ch.R

--- bots/corona-charts/corona-auto-ch/corona-auto-ch.R
+++ bots/corona-charts/corona-auto-ch/corona-auto-ch.R
@@ -295,10 +295,8 @@ bag_age_hosps  <- bag_hosp_age %>%
   select(datum, altersklasse_covid19, entries) %>%
   spread(altersklasse_covid19, entries) %>%
   mutate(`0–59` = `0 - 9` +  `10 - 19` + `20 - 29` +  `30 - 39` + `40 - 49` +  `50 - 59`, `60–79` = `60 - 69` +  `70 - 79`) %>%
-  select(datum, `0–59`,`60–79`, `80+`)
-
-# %>%
-#   slice(1:n()-1) #incomplete week results
+  select(datum, `0–59`,`60–79`, `80+`) %>%
+  slice(1:n()-1) #incomplete week results
 
 update_chart(id = "b3423b05ea50c39f8da718719ec3d161", data = bag_age_hosps)
 
