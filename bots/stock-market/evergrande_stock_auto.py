d67d11381f40a670c1c09cf157809eb442894dfb
Florian Seliger <seliger@kof.ethz.ch>
2021-09-27 17:31:24 +0200

    reorganizing stock market files

---
+++ bots/stock-market/evergrande_stock_auto.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import pandas as pd
+from datetime import date, datetime, timedelta
+import json
+import os
+import sys
+
+
+
+from helpers import *
+
+# Set Working Directory
+os.chdir(os.path.dirname(__file__))
+
+# set working directory, change if necessary
+os.chdir(os.path.dirname(__file__))
+
+tickers = ["3333.HK"] #Subtitute for the tickers you want
+df = yf.download(tickers,  start = "2021-01-04" , end = date.today() + timedelta(days = 1))
+q = df[['Close']].round(2)
+update_chart(id=\'82https://q.st.nzz.ch/editor/chart/0c12cfcee36676f241145bcc4b82095b', data=q)
+
+download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
+df.index = df.index.tz_localize(None)
+q2 = df[['Close']].round(2).to_clipboard()
+update_chart(id=\'82https://q.st.nzz.ch/editor/chart/588c5ac3ed81d1c4d5db6c104132125d', data=q2)
