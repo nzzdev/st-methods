ba3dbf82ff6aee9c525b249889d3dd15a602f248
Florian Seliger <seliger@kof.ethz.ch>
2021-09-29 09:40:22 +0200

    Update evergrande_stock_auto.py

--- bots/stock-market/evergrande_stock_auto.py
+++ bots/stock-market/evergrande_stock_auto.py
@@ -10,24 +10,16 @@ import sys
 from helpers import *
 
 # Set Working Directory
-# os.chdir(os.path.dirname(__file__))
-os.chdir('/Users/florianseliger/Documents/GitHub/st-methods/bots/stock-market/')
-
+os.chdir(os.path.dirname(__file__))
 
 tickers = ["3333.HK"] #Subtitute for the tickers you want
 df = yf.download(tickers,  start = "2021-01-04" , end = date.today() + timedelta(days = 1))
 q = df[['Close']].round(2)
-<<<<<<< HEAD
+
 update_chart(id= '0c12cfcee36676f241145bcc4b82095b', data=q)
-=======
-update_chart(id='0c12cfcee36676f241145bcc4b82095b', data=q)
->>>>>>> e9ebc5ec5f6c02fec0330853544366b52ad29d2b
 
 download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
 df.index = df.index.tz_localize(None)
 q2 = df[['Close']].round(2).to_clipboard()
-<<<<<<< HEAD
+
 update_chart(id= '588c5ac3ed81d1c4d5db6c104132125d', data=q2)
-=======
-update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=q2)
->>>>>>> e9ebc5ec5f6c02fec0330853544366b52ad29d2b
