243184a31d8c2f21aa8f6e16334d0bf011eff4d7
Florian Seliger <seliger@kof.ethz.ch>
2021-09-30 12:05:28 +0200

    corrected mistakes

--- bots/stock-market/evergrande_stock_auto.py
+++ bots/stock-market/evergrande_stock_auto.py
@@ -14,12 +14,14 @@ os.chdir(os.path.dirname(__file__))
 
 tickers = ["3333.HK"] #Subtitute for the tickers you want
 df = yf.download(tickers,  start = "2021-01-04" , end = date.today() + timedelta(days = 1))
-q = df[['Close']].round(2)
+df = df[['Close']].round(2)
+df.index = df.index.strftime('%Y-%m-%d')
 
-update_chart(id= '0c12cfcee36676f241145bcc4b82095b', data=q)
+update_chart(id='0c12cfcee36676f241145bcc4b82095b', data=df[['Close']])
 
-download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
+df = yf.download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
 df.index = df.index.tz_localize(None)
-q2 = df[['Close']].round(2).to_clipboard()
+df = df[['Close']].round(2)
+df.index = df.index.strftime('%Y-%m-%d')
 
-update_chart(id= '588c5ac3ed81d1c4d5db6c104132125d', data=q2)
+update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=df[['Close']])
