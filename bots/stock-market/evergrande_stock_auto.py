4d20079cfc27250123fcf24373d233b876cbd0b2
Florian Seliger <seliger@kof.ethz.ch>
2021-09-30 12:09:53 +0200

    Update evergrande_stock_auto.py

--- bots/stock-market/evergrande_stock_auto.py
+++ bots/stock-market/evergrande_stock_auto.py
@@ -26,12 +26,3 @@ df = df[['Close']].round(2)
 df.index = df.index.strftime('%Y-%m-%d')
 
 update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=df[['Close']])
-=======
-q = df[['Close']].round(2)
-update_chart(id='0c12cfcee36676f241145bcc4b82095b', data=q)
-
-download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
-df.index = df.index.tz_localize(None)
-q2 = df[['Close']].round(2).to_clipboard()
-update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=q2)
->>>>>>> Stashed changes
