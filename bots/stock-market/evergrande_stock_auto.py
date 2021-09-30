10a2c90871b824cc1f0903dae84393c28bca6024
Florian Seliger <seliger@kof.ethz.ch>
2021-09-30 12:07:36 +0200

    Update evergrande_stock_auto.py

--- bots/stock-market/evergrande_stock_auto.py
+++ bots/stock-market/evergrande_stock_auto.py
@@ -14,6 +14,7 @@ os.chdir(os.path.dirname(__file__))
 
 tickers = ["3333.HK"] #Subtitute for the tickers you want
 df = yf.download(tickers,  start = "2021-01-04" , end = date.today() + timedelta(days = 1))
+<<<<<<< Updated upstream
 df = df[['Close']].round(2)
 df.index = df.index.strftime('%Y-%m-%d')
 
@@ -25,3 +26,12 @@ df = df[['Close']].round(2)
 df.index = df.index.strftime('%Y-%m-%d')
 
 update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=df[['Close']])
+=======
+q = df[['Close']].round(2)
+update_chart(id='0c12cfcee36676f241145bcc4b82095b', data=q)
+
+download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
+df.index = df.index.tz_localize(None)
+q2 = df[['Close']].round(2).to_clipboard()
+update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=q2)
+>>>>>>> Stashed changes
