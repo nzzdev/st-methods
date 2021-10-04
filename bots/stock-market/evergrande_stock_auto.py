f09ff3446e0a976d3a58186c9f4f2ab75e58a621
Florian Seliger <seliger@kof.ethz.ch>
2021-10-04 11:10:49 +0200

    Update evergrande_stock_auto.py

--- bots/stock-market/evergrande_stock_auto.py
+++ bots/stock-market/evergrande_stock_auto.py
@@ -19,9 +19,9 @@ df.index = df.index.strftime('%Y-%m-%d')
 
 update_chart(id='0c12cfcee36676f241145bcc4b82095b', data=df[['Close']])
 
-df = yf.download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
-df.index = df.index.tz_localize(None)
-df = df[['Close']].round(2)
-df.index = df.index.strftime('%Y-%m-%d %H:%M:%S')
+#df = yf.download(tickers,  start = date.today() - timedelta(days = 1), end = date.today() + timedelta(days = 1), interval = "1m")
+#df.index = df.index.tz_localize(None)
+#df = df[['Close']].round(2)
+#df.index = df.index.strftime('%Y-%m-%d %H:%M:%S')
 
-update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=df[['Close']])
+#update_chart(id='588c5ac3ed81d1c4d5db6c104132125d', data=df[['Close']])
