60947d9d8b6e2aed197d940765fd080713c24cd4
Florian Seliger <seliger@kof.ethz.ch>
2021-10-04 19:06:22 +0200

    Update economic-real-time-data.R

--- bots/stock-market/economic-real-time-data.R
+++ bots/stock-market/economic-real-time-data.R
@@ -16,31 +16,6 @@ source("./helpers.R")
 
 
 
-####################
-# SECO indicator
-####################
-
-pg <- read_html("https://www.seco.admin.ch/seco/de/home/wirtschaftslage---wirtschaftspolitik/Wirtschaftslage/indikatoren/wwa.html")
-
-# get all the Excel (xls) links on that page:
-
-html_nodes(pg, xpath=".//a[contains(@href, '.xls')]") %>% 
-  html_attr("href") %>% 
-  sprintf("https://www.seco.admin.ch%s", .) -> excel_links
-
-url <- excel_links[1]
-GET(url, write_disk(tf <- tempfile(fileext = ".xls")))
-
-seco <- readWorksheetFromFile (tf, 3, startRow = 4, header = T)
-
-seco$Col3 <- ifelse(seco$Col2 < 10, paste0('0', as.character(seco$Col2)), as.character(seco$Col2))
-seco$KW <- paste0(as.character(seco$Col1), '-W', seco$Col3)
-
-q <- seco %>% dplyr::select(KW, WEA) %>% drop_na()
-
-#q-cli update
-update_chart(id = "c366afc02f262094669128cd054faf78", 
-             data = q)
 
 
 
