e911edcf7e98f391e351c302ffc3ec751516182b
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2021-12-06 14:32:39 +0100

    Update source and add column with boosters

--- bots/corona-charts/rki_vaccinations_states/rki_vaccinations_states.py
+++ bots/corona-charts/rki_vaccinations_states/rki_vaccinations_states.py
@@ -14,86 +14,47 @@ if __name__ == '__main__':
         # set working directory, change if necessary
         os.chdir(os.path.dirname(__file__))
 
-        # set source data and save
-        url = 'https://impfdashboard.de/static/data/germany_vaccinations_by_state.tsv'
+        # set source data
+        url = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/master/Aktuell_Deutschland_Impfquoten_COVID-19.csv'
+
+        # save data
         if not os.path.exists('data'):
             os.makedirs('data')
-        with open(os.path.join('data', 'germany_vaccinations_by_state.tsv'), 'wb') as f:
-            f.write(download_data(url).read())
-        url = 'https://impfdashboard.de/static/data/germany_vaccinations_timeseries_v2.tsv'
-        with open(os.path.join('data', 'germany_vaccinations_timeseries_v2.tsv'), 'wb') as f:
+        with open(os.path.join('data', 'Aktuell_Deutschland_Impfquoten_COVID-19.csv'), 'wb') as f:
             f.write(download_data(url).read())
 
-        # read vaccination and population data
-        df = pd.read_csv('./data/germany_vaccinations_by_state.tsv',
-                         delimiter='\t', encoding='utf-8')
-        dfpop = pd.read_csv('./pop_states_20203112.csv')
-        dfde = pd.read_csv('./data/germany_vaccinations_timeseries_v2.tsv',
-                           delimiter='\t', encoding='utf-8', usecols=['date', 'impf_quote_erst', 'impf_quote_voll'])
+        # read data
+        csv = './data/Aktuell_Deutschland_Impfquoten_COVID-19.csv'
+        df = pd.read_csv(csv, encoding='utf-8',
+                         usecols=['Datum', 'Bundesland', 'Impfquote_gesamt_voll', 'Impfquote_gesamt_boost'])
 
-        # merge df1 and dfpop
-        df = df.merge(dfpop, on='code')
-
-        # get date for chart notes and add one day from dfde
-        dfde = dfde.iloc[[-1]].reset_index(drop=True)
-        timestamp_str = dfde.loc[0, 'date']
+        # get date for chart notes and drop date column
+        timestamp_str = df.loc[0, 'Datum']
         timestamp_dt = datetime.strptime(
             timestamp_str, '%Y-%m-%d') + timedelta(days=1)
         timestamp_str = timestamp_dt.strftime('%-d. %-m. %Y')
+        df = df.drop(['Datum'], axis=1)
 
-        # get percentagedfdes for germany
-        de_first = (dfde['impf_quote_erst'].values[0] * 100).round(1)
-        de_full = (dfde['impf_quote_voll'].values[0] * 100).round(1)
-        de_total = dfde['impf_quote_erst'].values[0] + \
-            dfde['impf_quote_voll'].values[0]
-        de_total = (de_total * 100).round(1)
+        # extract Germany, drop first and last row
+        dfde = df.iloc[[0]].reset_index(drop=True)
+        df = df.drop(df.index[[-1, 0]])
 
-        # delete old df from memory
-        del [[dfde]]
-        gc.collect()
+        # sorty by fully vaccinated
+        df = df.sort_values(by='Impfquote_gesamt_voll', ascending=False)
 
-        # caluclate vaccination rates in new columns
-        df['Gesamt'] = ((df['vaccinationsTotal'] / df['pop']) * 100).round(1)
-        df['1. Dose'] = ((df['peopleFirstTotal'] / df['pop']) * 100).round(1)
-        df['2. Dose'] = ((df['peopleFullTotal'] / df['pop']) * 100).round(1)
-
-        # drop unwanted columns with absolute values
-        df.drop(['vaccinationsTotal', 'peopleFirstTotal',
-                'peopleFullTotal', 'pop'], axis=1, inplace=True)
-
-        # add data for Germany at the top
-        de = [{'code': 'Deutschland', 'Gesamt': de_total,
-               '1. Dose': de_first, '2. Dose': de_full}]
-        df = pd.concat(
-            [pd.DataFrame(de), df], ignore_index=True)
-
-        # create new df with data sorted by total vaccinations (except Germany)
-        df2 = df[df.code != 'Deutschland'].sort_values(
-            by='Gesamt', ascending=True)
-
-        # add Germany
-        df2 = df2.append(df[df.code == 'Deutschland'], ignore_index=True)
-
-        # delete old df from memory
-        del [[df]]
-        gc.collect()
-
-        # sort in descending order and bring Germany to top
-        df2 = df2[::-1]
+        # add Germany at the top
+        df = pd.concat([pd.DataFrame(dfde), df], ignore_index=True)
 
         # rename column headers
-        df2['code'] = df2['code'].str.replace("DE-TH", "Thüringen").str.replace("DE-HB", "Bremen").str.replace("DE-SL", "Saarland").str.replace("DE-RP", "Rheinland-Pfalz").str.replace("DE-BY", "Bayern").str.replace("DE-BE", "Berlin").str.replace("DE-SH", "Schleswig-Holstein").str.replace("DE-HH", "Hamburg").str.replace(
-            "DE-HE", "Hessen").str.replace("DE-NI", "Niedersachsen").str.replace("DE-BB", "Brandenburg").str.replace("DE-BW", "Baden-Württemberg").str.replace("DE-ST", "Sachsen-Anhalt").str.replace("DE-SN", "Sachsen").str.replace("DE-MV", "Mecklenburg-Vorpommern").str.replace("DE-NW", "Nordrhein-Westfalen")
-
-        # rename first column and set as index
-        df2 = df2.rename(columns={'code': 'Land'}).set_index('Land')
+        df = df.rename(columns={'Bundesland': 'Land', 'Impfquote_gesamt_voll': '2. Dose',
+                       'Impfquote_gesamt_boost': '3. Dose'}).set_index('Land')
 
         # show date in chart notes
         notes_chart = 'Stand: ' + timestamp_str
 
         # run function
         update_chart(id='245e5a30acb9ffa8e53b336e6bda032b',
-                     data=df2, notes=notes_chart)
+                     data=df, notes=notes_chart)
 
     except:
         raise
