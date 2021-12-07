737e68576b7358e8588a115f4c151e638b8ddf3e
Florian Seliger <fs.bwlvwl@googlemail.com>
2021-12-07 18:26:47 +0100

    Update corona-auto-ch.R

--- bots/corona-charts/corona-auto-ch/corona-auto-ch.R
+++ bots/corona-charts/corona-auto-ch/corona-auto-ch.R
@@ -648,7 +648,7 @@ title_vacc_kant <- paste("In", head(vacc_ch_persons_kant$kt, 1), "sind am meiste
 
 update_chart(id = "54381c24b03b4bb9d1017bb91511e21d",
              data = vacc_ch_persons_kant,
-             notes = paste0("In der Schweiz wurden nur wenige Impfungen mit dem Impfstoff von Johnson & Johnson durchgeführt. Diese Impfungen erfordern eine statt zwei Impfdosen und sind daher in der Kategorie «Nur doppelt geimpft» enthalten. <br>Stand: ", ch_vacc_date), 
+             notes = paste0("In der Schweiz wurden nur wenige Impfungen mit dem Impfstoff von Johnson & Johnson durchgeführt. Diese Impfungen erfordern eine statt zwei Impfdosen und sind daher in der Kategorie «Nur doppelt geimpft» enthalten. Auch Genesene, die eine Impfdoses erhalten haben, sind dort aufgeführt.<br>Stand: ", ch_vacc_date), 
              title = title_vacc_kant)
 
 ### Schweiz geimpft nach Altersgruppen
@@ -680,7 +680,7 @@ title <- paste("Rund", round(vacc_ch_age[vacc_ch_age$Altersklasse == "80+",]$`Bo
 
 update_chart(id = "674ce1e7cf4282ae2db76136cb301ba1", 
              data = vacc_ch_age, 
-             notes = paste0("Die Zahlen werden wöchentlich aktualisiert. In der Schweiz wurden nur wenige Impfungen mit dem Impfstoff von Johnson & Johnson durchgeführt. Diese Impfungen erfordern eine statt zwei Impfdosen und sind daher in der Kategorie «Nur doppelt geimpft» enthalten. <br>Stand: ", gsub("\\b0(\\d)\\b", "\\1", vacc_ch_age_date)),
+             notes = paste0("In der Schweiz wurden nur wenige Impfungen mit dem Impfstoff von Johnson & Johnson durchgeführt. Diese Impfungen erfordern eine statt zwei Impfdosen und sind daher in der Kategorie «Nur doppelt geimpft» enthalten. Auch Genesene, die eine Impfdoses erhalten haben, sind dort aufgeführt.<br>Stand: ", gsub("\\b0(\\d)\\b", "\\1", vacc_ch_age_date)),
              title = title)
 
 #### Vaccination, delivered, received ####
