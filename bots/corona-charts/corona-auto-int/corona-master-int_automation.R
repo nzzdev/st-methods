171b76f642149d50fd12e679cde4a59bcb1c0fcb
Florian Seliger <seliger@kof.ethz.ch>
2021-12-09 12:27:58 +0100

    Update corona-master-int_automation.R

--- bots/corona-charts/corona-auto-int/corona-master-int_automation.R
+++ bots/corona-charts/corona-auto-int/corona-master-int_automation.R
@@ -311,19 +311,17 @@ full_vacc <- vaccination %>%
   drop_na(`Vollständig geimpft, in % der Bev.`) %>%
   group_by(Land) %>%
   mutate(Datum = as.Date(Datum, "%d.%m.%Y")) %>%
-  filter(Land != 'World', pop > 1000000) %>%
+  filter(Land != 'World', pop > 1000000, `Vollständig geimpft, in % der Bev.` > 0) %>%
   slice_max(order_by = Datum, n = 1) %>%
   arrange(desc(`Vollständig geimpft, in % der Bev.`)) %>%
   select(Land, `Vollständig geimpft, in % der Bev.`) %>%
   mutate(`Vollständig geimpft, in % der Bev.` = round(`Vollständig geimpft, in % der Bev.`, 1)) %>%
   ungroup() 
 
-data <- full_vacc %>% slice(1:20)
-
 title <- paste("In", head(full_vacc$Land, 1), "sind", round(head(full_vacc$`Vollständig geimpft, in % der Bev.`, 1)), "Prozent der Einwohner vollständig geimpft" )
 
 notes <- paste0("Länder mit mehr als 1 Million Einwohnern. Eine Person muss in der Regel zwei Dosen geimpft bekommen, um als vollständig geimpft zu gelten. GB = Grossbritannien, VAE = Vereinigte Arabische Emirate.<br>Stand: ", gsub("\\b0(\\d)\\b", "\\1", format(max(vaccination$Datum), format = "%d. %m. %Y")))
-update_chart(id = 'a15febf1b611cd45c56db4f85b1ea73b', data = data, notes = notes, title = title)
+update_chart(id = 'ee2ce1d4bd795db54ef74a9ed8abb147', data = full_vac, notes = notes, title = title)
 
 
 booster <- vaccination %>%
