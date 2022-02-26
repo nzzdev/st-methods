23394ecd737fd303e4fac62ede2dbd01b4b6d111
Simon Huwiler <webmaster@simonhuwiler.ch>
2022-02-26 16:35:20 +0100

    add ukraine updater

---
+++ bots/ukraine-map/src/geojson_export.py
@@ -0,0 +1,50 @@
+# %% [markdown]
+# # Geojson export
+
+# %%
+import pandas as pd
+import json
+
+# %%
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR28WE_2xWMoUvFIItA_rQDu0-nBnmIByy3T3H1bcZCeni1FWAxVLUFxml6sBhYCTWxsVoLNSaOTrY8/pub?gid=0&single=true&output=csv'
+
+def create_geojson(export_path):
+
+    df = pd.read_csv(url)
+
+    data = {
+    "type": "FeatureCollection",
+    "features": []
+    }
+
+    df['grey'] = df['grey'].fillna(0)
+    df = df.fillna('')
+
+    for i, row in df[df['_visible'] == 1].iterrows():
+        coords = row['latlon'].replace(' ', '').split(',')
+
+        if len(coords) < 2:
+            raise ValueError('Koordinaten falsch: %s' % list(row))
+
+        data['features'].append({
+            "type": "Feature",
+            "properties": {
+                "date": row['date'],
+                "place": row['place'],
+                "title": row['title'],
+                "text": row['text'],
+                "url": row['url'],
+                "imgurl": row['imgurl'],
+                "type": row['type'],
+                "source": row['source'],
+                "sourceurl": row['sourceurl'],
+                "grey": int(row['grey']),
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [float(coords[1]), float(coords[0])]
+            }
+        })
+
+    with open(export_path, 'w', encoding='UTF-8') as f:
+        json.dump(data, f, ensure_ascii=False)
---
+++ bots/ukraine-map/src/q_upload.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import os
+import geojson_export
+import helpers
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', type=str, required=True, help='"staging" or "production"')
+args = parser.parse_args()
+print("Update %s" % args.p)
+
+# Set working directory
+workingdir = Path(__file__).parents[1].absolute()
+os.chdir(workingdir)
+
+export_path = Path(workingdir / 'annotations.json')
+
+geojson_export.create_geojson(export_path)
+
+if args.p.lower() == 'staging':
+    chartid = 'ecf1db20c4ca6dcf6e7909906709f3f7'
+elif args.p.lower() == 'production':
+    chartid = 'xxx'
+else:
+    raise Exception('Unknown platform: %s' % args.p)
+
+files = [{
+        "file": {
+            'path': str(export_path)
+        }
+    }]
+
+helpers.update_chart(
+    id = chartid,
+    files = files
+)
