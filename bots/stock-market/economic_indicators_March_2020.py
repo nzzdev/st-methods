50879f3932bf5bab74b9321191f318fee60e65ad
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-04-04 17:30:29 +0200

    Update economic_indicators_March_2020.py

--- bots/stock-market/economic_indicators_March_2020.py
+++ bots/stock-market/economic_indicators_March_2020.py
@@ -119,6 +119,7 @@ benzin.loc[benzin['95'].isna(), '95'] = benzin['98']
 benzin = benzin[['Reiseziel', '95']]
 benzin.rename(columns = {'95': 'Benzinpreis'}, inplace = True)
 benzin.sort_values(by = 'Benzinpreis', ascending = False, inplace = True)
+benzin.set_index('Reiseziel', inplace = True)
 
 update_chart(id = '4359e80ee2738a55d5f04f1409ffebf1', data = benzin)
 
