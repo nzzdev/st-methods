7f96ece590be9fbf4209217d86cc36a155ee235f
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-05-17 09:44:52 +0200

    new

--- bots/stock-market/economic_indicators_March_2020.py
+++ bots/stock-market/economic_indicators_March_2020.py
@@ -187,8 +187,12 @@ page = requests.get(adac)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-text = soup.find_all('b')[6].text.strip()
-price_e10 = pd.to_numeric(re.findall(r'\d+\,\d+', text)[0].replace(',','.'))
+try:
+        text = soup.find_all('b')[6].text.strip()
+        price_e10 = pd.to_numeric(re.findall(r'\d+\,\d+', text)[0].replace(',','.'))
+except IndexError:
+        text = soup.find_all('b')[5].text.strip()
+        price_e10 = pd.to_numeric(re.findall(r'\d+\,\d+', text)[0].replace(',','.'))
 
 fuel_prices_old = pd.read_csv('./Benzinpreise.csv')
 fuel_prices_old_de = pd.read_csv('./Benzinpreise_de.csv')
