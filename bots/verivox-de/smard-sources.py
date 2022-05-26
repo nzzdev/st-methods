a64b1e0f21d322b2cb469578df4ef3baf4db4363
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2022-05-26 20:46:08 +0200

    Convert megawatt to terawatt

--- bots/verivox-de/smard-sources.py
+++ bots/verivox-de/smard-sources.py
@@ -82,6 +82,9 @@ if __name__ == '__main__':
         perc_gas = (df_perc['Gas'].iloc[-1]*100).round(0).astype(int)
         title_chart = f'{perc_gas} Prozent des Stroms stammen derzeit aus Erdgas'
 
+        # convert to terawatt
+        df = df.div(1000000)
+
         # convert DatetimeIndex to string
         df.index = df.index.strftime('%Y-%m-%d')
 
