b7934aa53a5d9be260a1e7e9139b4e5e7dd7e8a1
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2022-06-28 09:56:42 +0200

    Update y-axis

--- bots/verivox-de/gas-dashboard.py
+++ bots/verivox-de/gas-dashboard.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
         gas_y = 10
         super_y = 1.6
         storage_ytick = [0, 20, 40, 60, 80]
-        gas_ytick = [10, 15, 20, 25]
+        gas_ytick = [11, 14, 17, 20, 23]
         super_ytick = [1.6, 1.8, 2.0, 2.2, 2.4]
 
         # change decimal seperator
