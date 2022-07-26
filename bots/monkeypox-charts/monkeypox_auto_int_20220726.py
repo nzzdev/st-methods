de415b520e8c433f17914fcfbd42343aa187b66f
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-07-26 17:27:46 +0200

    Update monkeypox_auto_int_20220726.py

--- bots/monkeypox-charts/monkeypox_auto_int_20220726.py
+++ bots/monkeypox-charts/monkeypox_auto_int_20220726.py
@@ -4,6 +4,7 @@ import pycountry
 import gettext
 from datetime import datetime
 import os
+import country_converter as coco
 german = gettext.translation(
     'iso3166', pycountry.LOCALES_DIR, languages=['de'])
 german.install()
@@ -38,7 +39,6 @@ df['Country'] = df['Country'].str.strip()
 df['Country_sum'] = df['Country']
 df.loc[df['Country'].isin(['England', 'Scotland', 'Wales', 'Northern Ireland',
 'United Kingdom']), 'Country_sum'] = 'United Kingdom'
-df.loc[df['Country'].isin(['France', 'French Guiana']), 'Country_sum'] = 'France'
 
 df = df.groupby('Country_sum')['Total'].sum().reset_index()
 
@@ -50,74 +50,52 @@ df['Country'] = df['Country'].str.replace('Iran', 'Iran, Islamic Republic of')
 df['Country'] = df['Country'].str.replace('Czech Republic', 'Czechia')
 
 # remove unofficial countries
-df = df[df['Country']!='Kosovo']
+#df = df[df['Country']!='Kosovo']
 
-for index, row in df.iterrows():
-    print(_(row['Country']))
 
 df['Land'] = df['Country'].apply(lambda x: _(x))
 
-
-df['Land'] = _(df['Country'])
-df['Flagge'] = df['Country'].apply(get_flag)
 df['Land'] = df['Land'].str.replace('Iran, Islamische Republik', 'Iran')
 df['Land'] = df['Land'].str.replace('Russia', 'Russland')
+df['Land'] = df['Land'].str.replace('Bosnia And Herzegovina', 'Bosnien-H.')
+df['Land'] = df['Land'].str.replace('South Korea', 'Südkorea')
 
 
+df = df[['Country', 'Land', 'Total']]
+df['ID'] = df['Country'].apply(lambda x: coco.convert(names=x, to='ISO3', not_found=None))
 
-df = df[['Country', 'Land', 'Flagge', 'Bestätigt', 'Verdacht', 'Total']]
 
-# save csv
-#now = datetime.now()
-#time_now = now.strftime("%Y%m%d-%Hh%M")
-#df.to_csv('data/'+ time_now+'.csv', index=False)
 
 # set date for charts
 date_notes = 'Stand: '+ datetime.now().strftime("%-d. %-m. %Y")
 
-# export for q world map
-ids = pd.read_csv('country_ids.csv')
-df_worldmap = df.copy()
-
-# Rename countries to match world map ids
-df_worldmap['Country'] = df_worldmap['Country'].str.replace(
-    'United States', 'United States of America')
-df_worldmap['Country'] = df_worldmap['Country'].str.replace(
-    'Iran, Islamic Republic of', 'Iran')
-df_worldmap['Country'] = df_worldmap['Country'].str.replace(
-    'Bahamas','The Bahamas')
-df_worldmap['Country'] = df_worldmap['Country'].str.replace(
-        'Serbia','Republic of Serbia')
+ids = pd.read_csv('/Users/florianseliger/Documents/GitHub/st-methods/bots/monkeypox-charts/q_countries.csv')
 
 # merge df with ids
-df_worldmap = df_worldmap.rename(columns={'Country': 'ID', 'Total': 'Wert'})
-df_worldmap = ids.merge(df_worldmap[['ID', 'Wert']], how='left').sort_values(
-    'Wert', ascending=False)
+df = ids.merge(df, on = 'ID', how = 'left')
+df['Wert'] = df['Total'].fillna("")
 
-# to check locally if all countries were recognised by q_worldmap
-#print('Total Worldmap:', str(df_worldmap['Wert'].sum()))
-#print('Total DF:', str(df['Total'].sum()))
-df_worldmap.to_csv('test_worldmap.csv', index=False)
+pop = pd.read_csv('https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2022_TotalPopulationBySex.zip')
+pop = pop[pop['Time'].isin([2021])]
+pop = pop[['ISO3_code', 'PopTotal']]
 
-# Countries that are not in q worldmap ids
-ignore_lst = ['Cayman Islands', 'Gibraltar', 'Singapore']
 
-# check if all countries were merged
-if df_worldmap['Wert'].sum() != df[~df['Country'].isin(ignore_lst)]['Total'].sum():
-    raise ValueError(
-        'Some country names do not correspond with our world map ids. Please rename manually.')
+df = df.merge(pop, left_on = 'ID', right_on = 'ISO3_code', how = 'left' )
+df['Fälle pro 1 Mio. Einwohner'] = round(df['Total']*1000/df['PopTotal'], 1)
+df.rename(columns = {'Total': 'Fälle gesamt'}, inplace = True)
 
-df_worldmap = df_worldmap.sort_values('ID', key=lambda col: col.str.lower())
-df_worldmap['Wert'] = df_worldmap['Wert'].fillna("")
+
+
+
 
 id_worldmap = '4acf1a0fd4dd89aef4abaeefd0b6f4dc'  # linked in article
 update_chart(id=id_worldmap, 
-            data=df_worldmap,
+            data=df[['ID', 'Wert']],
             notes = date_notes)
 
 # export for q table
-df_q_table = df[['Land', 'Flagge', 'Bestätigt', 'Verdacht', 'Total']].rename(
-    columns={'Land': '', 'Flagge': ''})
+df_q_table = df[['Land', 'Fälle gesamt', 'Fälle pro 1 Mio. Einwohner']].rename(
+    columns={'Land': ''})
 
 id_q_table = '4acf1a0fd4dd89aef4abaeefd0da5ac6'  # linked in article
 update_chart(id=id_q_table, 
