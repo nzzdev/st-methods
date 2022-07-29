43a7e902def801f52702824eaae078833fefb310
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-07-29 13:11:42 +0200

    Update economic_indicators_March_2020.py

--- bots/stock-market/economic_indicators_March_2020.py
+++ bots/stock-market/economic_indicators_March_2020.py
@@ -217,7 +217,9 @@ benzin = benzin[['Reiseziel', '95']]
 benzin.rename(columns={'95': 'Benzinpreis'}, inplace=True)
 benzin.sort_values(by='Benzinpreis', ascending=False, inplace=True)
 
-update_chart(id = '4359e80ee2738a55d5f04f1409ffebf1', data = benzin[['Reiseziel', 'Benzinpreis']])
+benzin_table = benzin[['Reiseziel', 'Benzinpreis']]
+
+update_chart(id = '4359e80ee2738a55d5f04f1409ffebf1', data = benzin_table)
 
 session = requests_html.HTMLSession()
 r = session.get(url)
