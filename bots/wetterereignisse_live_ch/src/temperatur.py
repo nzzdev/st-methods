ebb86fd6140c5a0d93a491bf385c49c5bdddf875
webmaster@simonhuwiler.ch <webmaster@simonhuwiler.ch>
2022-08-03 13:38:58 +0200

    update

--- bots/wetterereignisse_live_ch/src/temperatur.py
+++ bots/wetterereignisse_live_ch/src/temperatur.py
@@ -5,7 +5,6 @@ from helpers import *
 import pandas as pd
 import requests
 from datetime import timezone, datetime, timedelta
-from datetime import datetime
 import os
 
 # Set Working Directory
@@ -41,7 +40,7 @@ df = pd.concat([
 
 
 df['date_str'] = df['date'].apply(lambda x: x.astimezone('Europe/Berlin').strftime("%-d. %-m. %Y, %H.%M"))
-df['date'] = df['date'].apply(lambda x: x.astimezone('Europe/Berlin').strftime("%Y-%m-%d %H:%M"))
+# df['date'] = df['date'].apply(lambda x: x.astimezone('Europe/Berlin').strftime("%Y-%m-%d %H:%M"))
 
 df = pd.pivot_table(df, index=['date', 'date_str'], columns='type', values='temp')
 df['Hitzerekord von 2003 (36°)'] = 36
@@ -49,7 +48,23 @@ df['Hitzerekord von 2003 (36°)'] = 36
 df = df[['Stundenminimum', 'Stundenmaximum', 'Hitzerekord von 2003 (36°)']]
 
 current_temp = str(df.reset_index().iloc[-1]['Stundenmaximum']).replace('.', ',')
-current_hour = datetime.strptime(df.reset_index().iloc[-1]['date'], '%Y-%m-%d %H:%M').hour
+# current_hour = datetime.strptime(df.reset_index().iloc[-1]['date'], '%Y-%m-%d %H:%M').hour
+current_hour = df.reset_index().iloc[-1]['date'].hour
+
+# Annotationen hinzufügen: Höchstwerte der letzten beiden Tagen + aktuell
+now = df.reset_index().iloc[-1]['date'].normalize()
+events = []
+for i in range(0, 3):
+    new_date = now - timedelta(days = i)
+    df_day = df[df.index.get_level_values('date').normalize() == (now - timedelta(days = i))].sort_values('Stundenmaximum', ascending=False).reset_index()
+    n_max = df_day.iloc[0]
+    events.append({
+        'type': 'point',
+        'date': n_max.date.strftime('%Y-%m-%d %H:%M'),
+        'label': '%s° Celsius am %s' % (str(n_max.Stundenmaximum).replace('.', ','), n_max.date.strftime("%d. %B"))
+    })
+
+# print(json.dumps(events, indent=2))
 
 update_chart(
     id = 'd0be298e35165ab925d72923352cad8b',
@@ -57,20 +72,21 @@ update_chart(
     title = "In Zürich wird es heute wieder heiss",
     #title = "Aktuell beträgt die Temperatur in Zürich %s Grad" % current_temp,
     notes="Messstation Zürich Fluntern<br />Zuletzt aktualisiert: %s Uhr" % df.reset_index().iloc[-1]['date_str'],
-    events=[
-        {
-            'type': 'point',
-            'date': '2022-08-02 18:00',
-            'label': '29,2° Celsius am 2. August'
-        },
-        {
-            'type': 'point',
-            'date': '2022-08-01 17:00',
-            'label': '30,7° Celsius am 1. August'
-        },
-        {
-            'type': 'point',
-            'date': df.reset_index().iloc[-1]['date'],
-            'label': "Aktuell %s° (%s Uhr)" % (current_temp, current_hour)
-        }]
+    events = events
+    # [
+    #     {
+    #         'type': 'point',
+    #         'date': '2022-08-02 18:00',
+    #         'label': '29,2° Celsius am 2. August'
+    #     },
+    #     {
+    #         'type': 'point',
+    #         'date': '2022-08-01 17:00',
+    #         'label': '30,7° Celsius am 1. August'
+    #     },
+    #     {
+    #         'type': 'point',
+    #         'date': df.reset_index().iloc[-1]['date'],
+    #         'label': "Aktuell %s° (%s Uhr)" % (current_temp, current_hour)
+    #     }]
 )
