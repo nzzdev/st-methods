fba625bfc370068586839a2cdf7df0878b50e95f
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-09-05 17:53:40 +0200

    Update economic_indicators_March_2020.py

--- bots/stock-market/economic_indicators_March_2020.py
+++ bots/stock-market/economic_indicators_March_2020.py
@@ -204,10 +204,10 @@ benzin['98'] = pd.to_numeric(benzin['98']).round(2)
 benzin.loc[benzin['95'].isna(), '95'] = benzin['98']
 benzin = benzin[['Reiseziel', '95']]
 benzin.rename(columns={'95': 'Benzinpreis'}, inplace=True)
-benzin.sort_values(by='Benzinpreis', ascending=False, inplace=True)
 
 benzin_table = benzin[['Reiseziel', 'Benzinpreis']]
-benzin_table['Benzinpreis'] = benzin_table['Benzinpreis'].astype(str)
+benzin_table.sort_values(by='Benzinpreis', ascending=False, inplace=True)
+benzin_table.rename_axis(None, axis=1, inplace = True)
 
 notes = "Die Datenbasis ist in den einzelnen Ländern sehr unterschiedlich. Ausserdem gibt es teilweise einen grossen Verzug bei den Preismeldungen. Die Preise sind daher als Grössenordnung zu verstehen. Für Finnland wird der Preis für einen Liter bleifrei 98 ausgewiesen. Die Datenbasis ist in den einzelnen Ländern sehr unterschiedlich. Ausserdem gibt es teilweise einen grossen Verzug bei den Preismeldungen. Die Preise sind daher als Grössenordnung zu verstehen. Für Finnland wird der Preis für einen Liter bleifrei 98 ausgewiesen."
 update_chart(id = '4359e80ee2738a55d5f04f1409ffebf1', data = benzin_table, notes = notes)
