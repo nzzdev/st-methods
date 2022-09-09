93a5a7edc6d7fac91258acb5fa3689e198cc91eb
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-09-09 15:33:20 +0200

    Update economic_indicators_March_2020.py

--- bots/stock-market/economic_indicators_March_2020.py
+++ bots/stock-market/economic_indicators_March_2020.py
@@ -380,15 +380,20 @@ df_gas = df_gas.reset_index(level=0)
 # run Q function
 update_chart(id='1dda540238574eac80e865faa0ddbafc', data=df_gas)
 
-#oil = pd.read_csv('https://www.marketwatch.com/investing/future/brn00/downloaddatapartial?startdate=01/03/2022%2000:00:00&enddate=09/06/2022%2023:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false&countrycode=uk')
+oil = pd.read_csv('https://www.marketwatch.com/investing/future/brn00/downloaddatapartial?startdate=01/01/2021%2000:00:00&enddate=09/08/2022%2000:00:00&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false&countrycode=uk')
 
-oil = df['Close']['BZX22.NYM'][df.index >=
-                          '2022-01-01'].to_frame().dropna().reset_index(level=0)
+oil['Date'] = pd.to_datetime(oil['Date'], format = '%m/%d/%Y')
+oil.sort_values(by = 'Date', ascending = True, inplace = True)
+
+
+#oil = df['Close']['BZX22.NYM'][df.index >=
+ #                         '2022-01-01'].to_frame().dropna().reset_index(level=0)
 #oil['Jahresdurchschnitt 2019'] = df['Close']['BZX22.NYM'][(
  #   df.index >= '2019-01-01') & (df.index <= '2019-12-31')].mean()
+oil = oil[['Date', 'Close']]
+
 oil.rename(columns={oil.columns[1]: '2022'}, inplace=True)
 #oil.index = oil.index.strftime('%Y-%m-%d')
-oil = oil[['Date', '2022']]
 
 update_chart(id='c6aec0c9dea84bcdef43b980cd4a7e3f', data=oil)
 
