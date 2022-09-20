2722bbadc08c946a123e8dbf95a6e090c1be1e73
webmaster@simonhuwiler.ch <webmaster@simonhuwiler.ch>
2022-09-20 09:23:23 +0200

    add headers

--- bots/crawl_ch/crawl_ch.py
+++ bots/crawl_ch/crawl_ch.py
@@ -15,12 +15,21 @@ sys.path.append(os.path.dirname((os.path.dirname(__file__))))
 # Set Working Directory
 os.chdir(os.path.dirname(__file__))
 
+# Headers for HTTP_Request
 headers = {
-    'accept': '*/*',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
-    'origin': 'https://www.coop.ch',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
+    'accept-language': 'de-DE,de;q=0.9',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "macOS",
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 }
 
