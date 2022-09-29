e9a4f98c4bd5ad648ef8084f2de1861dd3b81181
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2022-09-29 16:01:32 +0200

    Update chart with gas consumption

--- bots/verivox-de/bnetza-verbrauch.py
+++ bots/verivox-de/bnetza-verbrauch.py
@@ -20,45 +20,55 @@ if __name__ == '__main__':
         }
 
         # download data
-        url = 'https://www.bundesnetzagentur.de/DE/Fachthemen/ElektrizitaetundGas/Versorgungssicherheit/aktuelle_gasversorgung/_svg/download.csv?nn=1059464&svgid=1067070&view=renderCSV'
+        url = 'https://www.bundesnetzagentur.de/_tools/SVG/js2/_functions/csv_export.html?view=renderCSV&id=1081208'
         resp = download_data(url, headers=fheaders)
         csv_file = resp.text
 
         # read csv, drop last column with weather and convert to str
         df = pd.read_csv(io.StringIO(csv_file), encoding='utf-8',
-                         sep=';', decimal=',', index_col=0)
-        df = df.iloc[:, :-1]
-        df.iloc[:, 0] = df.iloc[:, 0].astype(
-            str).str.replace('0.0', '', regex=False)
+                         sep=';', decimal=',', index_col=None)
+        df = df.drop(df.columns[[2, 3]], axis=1)
 
-        # convert month strings to datetime
-        df.index = df.index + ' 2022'
-        df.index = df.index.str.replace('Mrz', 'Mar').str.replace(
-            'Mai', 'May').str.replace('Okt', 'Oct').str.replace('Dez', 'Dec', regex=False)
-        df.index = pd.to_datetime(df.index, errors='coerce')
+        # convert week number to datetime (and fix first and last week)
+        df.iloc[:, 0] = df.iloc[:, 0].astype(str).str.zfill(2)
+        df.iloc[:, 0] = df.iloc[:, 0] + '-2022'
+        df.iloc[:, 0] = pd.to_datetime(
+            '0-' + df.iloc[:, 0], format='%w-%U-%Y') + pd.DateOffset(days=7)
+        df.iloc[-1, 0] = df.iloc[-1, 0] - pd.DateOffset(days=1)
+        df.iloc[0, 0] = df.iloc[0, 0] - pd.DateOffset(days=8)
+        # df['.'] = '2022-W' + df['.']
+
+        # rename and reorder columns
+        df = df.rename(columns={
+                       df.columns[0]: 'Datum', df.columns[2]: '', df.columns[3]: 'Höchst-/Tiefststand¹'})
+        df = df[['Datum', '', 'Höchst-/Tiefststand¹', '2022']]
 
         # add new column with goal
-        df = df.assign(goal=['', '', '', '', '', '', '34.34',
-                       '32.98', '38.505', '62.22', '93.245', '106.76'])
-        df = df.rename(columns={'goal': 'EU-Ziel¹'})
+        df = df.assign(goal=['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '1334.5', '1283.5', '1239.3', '1163.65', '1213.8', '1165.35', '1232.5',
+                       '1335.35', '1374.45', '1301.35', '1399.95', '1539.35', '1566.55', '1912.5', '2257.6', '2211.7', '2277.15', '2606.1', '2977.55', '2893.4', '3407.65', '3488.4', '3664.35', '3146.7', '3611.65', '2710.65'])
+        df = df.rename(columns={'goal': 'EU-Ziel²'})
+
+        # convert date column to index
+        df = df.set_index('Datum')
 
         # create timestamp for chart notes
-        df.iloc[:, 0] = df.iloc[:, 0].replace('', np.nan)
-        timestamp = df.iloc[:, 0].notna()[::-1].idxmax()
-        timestamp_str = timestamp.strftime("%B %Y")
-        chart_notes = f'Gemäss Gas-Notfallplan der EU sollen alle Länder ihren Verbrauch bis März 2023 um 15 Prozent senken.<br><br>Stand: {timestamp_str}'
+        df.iloc[:, 2] = df.iloc[:, 2].replace('', np.nan)
+        timestamp = df.iloc[:, 2].notna()[::-1].idxmax()
+        timestamp_notes = timestamp + pd.DateOffset(days=1)
+        timestamp_notes_str = timestamp.strftime('%-d. %-m. %Y')
+        chart_notes = f'¹ Maximum/Minimum des Verbrauchs 2018-2021.<br>² Gemäss Gas-Notfallplan der EU sollen alle Länder ihren Verbrauch bis März 2023 um 15 Prozent gegenüber 2021 senken.<br><br>Stand: {timestamp_notes_str}'
 
         # create dynamic chart title
-        currenty = df.iloc[:, 0].loc[~df.iloc[:, 0].isnull()].iloc[-1]
-        lasty = df.iloc[:, 1].loc[timestamp]
-        diffy = (float(currenty) - float(lasty))
+        currenty = df.iloc[:, 2].loc[~df.iloc[:, 2].isnull()].iloc[-1]
+        eugoaly = df.iloc[:, 3].loc[timestamp]
+        diffy = (float(currenty) - float(eugoaly))
         if diffy < 2 and diffy > -2:
-            chart_title = 'Gasverbrauch wieder auf Vorjahresniveau'
+            chart_title = 'Gasverbrauch im Soll'
         elif diffy >= 2:
-            chart_title = 'Deutschland verbraucht mehr Gas als im Vorjahr'
+            chart_title = 'Gasverbrauch über dem Soll'
         else:
-            chart_title = 'Deutschland verbraucht weniger Gas als im Vorjahr'
-        df.iloc[:, 0] = df.iloc[:, 0].replace(np.nan, '')
+            chart_title = 'Gasverbrauch unter dem Soll'
+        df.iloc[:, 2] = df.iloc[:, 2].replace(np.nan, '')
 
         # run Q function
         update_chart(id='df32c18d6ba570e13338190d2936f17d',
