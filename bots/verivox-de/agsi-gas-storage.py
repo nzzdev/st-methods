eec0bdd7a6ce22da394d884985743777d06a3b4b
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2022-10-15 16:59:50 +0200

    Move permanent code to new line

--- bots/verivox-de/agsi-gas-storage.py
+++ bots/verivox-de/agsi-gas-storage.py
@@ -128,6 +128,8 @@ if __name__ == '__main__':
         dfnew.to_csv(f'./data/{todaystr}-gasspeicher.csv',
                      encoding='utf-8', index=True)
         dfnew = dfnew.reset_index(level=0)
+
+        # drop trend
         dfnew = dfnew.drop('Trend', axis=1)
 
         # convert date column to datetime
