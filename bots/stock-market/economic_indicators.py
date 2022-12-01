3985a603653778f9c4cd299eb4fff615a90b9210
Florian Seliger <fs.bwlvwl@googlemail.com>
2022-12-01 16:51:03 +0100

    Update economic_indicators.py

--- bots/stock-market/economic_indicators.py
+++ bots/stock-market/economic_indicators.py
@@ -771,12 +771,12 @@ update_chart(id='9039ce8be0b7e1650165751c47d993d4',
                  data=df_1)
 
 
-df_2 = yf.download(tickers,  period = "7d", interval = "1m")
+df_2 = yf.download(tickers,  period = "4d", interval = "1m")
 df_2 = df_2['Close'].to_frame().dropna().reset_index(level = 0)
 
 df_2['Datetime'] = pd.to_datetime(df_2['Datetime']).dt.strftime('%Y-%m-%d %H:%M')
 
-update_chart(id='9039ce8be0b7e1650165751c47d571bc',
+update_chart(id='53dcf67825b59f9a226adacf9ea2ca2d',
               data=df_2)
 
 
