cad5b706b4c050ad46857bef27c95872b4410eeb
Florian Seliger <fs.bwlvwl@googlemail.com>
2023-01-23 15:57:51 +0100

    Update main.py

--- bots/strom-charts-ch/main.py
+++ bots/strom-charts-ch/main.py
@@ -37,4 +37,5 @@ df_bfe = bfe.get_bfe()
   Update q.config
 """
 
+update_article.run(df_futures, df_spotmarket, df_atomstrom_fr, df_speicherseen, df_bfe)
 update_dashboard.run(df_bfe, df_spotmarket, df_speicherseen)
