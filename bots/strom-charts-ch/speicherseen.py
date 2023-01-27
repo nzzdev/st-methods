81dcdf7066bb6929bb1bc011e53ee2f146fc12ab
Florian Seliger <fs.bwlvwl@googlemail.com>
2023-01-27 10:57:01 +0100

    Update speicherseen.py

--- bots/strom-charts-ch/speicherseen.py
+++ bots/strom-charts-ch/speicherseen.py
@@ -7,24 +7,46 @@ pd.options.mode.chained_assignment = None
 
 def get_speicherseen():
     # read data
-    url  = 'https://www.uvek-gis.admin.ch/BFE/ogd/17/ogd17_fuellungsgrad_speicherseen.csv'
-    df = pd.read_csv(url)
+    url  = 'https://www.energiedashboard.admin.ch/api/v2/fuellungsgrad-speicherseen'
+    
+    response = urlopen(url)
 
-    columns = [col[:-19] for col in df.columns.to_list()[1:]]
-    columns.insert(0, 'Datum')
-    df.columns = columns
-    df['Datum'] = pd.to_datetime(df['Datum'])
+    data_json = json.loads(response.read())
+    
+    totalCH = data_json['totalCH']
+    
+    df = pd.DataFrame(totalCH['entries'])
+    
+    df_w = df[['date', 'fiveYearMin', 'fiveYearMax', 'fiveYearMittelwert', 'speicherstandProzent']]
+
+    df_w = df_w.dropna()
+
+    df_w['date'] = pd.to_datetime(df_w['date'])
+
+    df_w.rename(columns = {'fiveYearMin': '', 
+                           'fiveYearMax': 'Minimum/Maximum¹',
+                           'fiveYearMittelwert': '5-Jahres-Mittelwert',
+                           'landesverbrauch': 'Füllstand'
+                           }, inplace = True)
+
+    
+    #columns = [col[:-19] for col in df.columns.to_list()[1:]]
+    #columns.insert(0, 'Datum')
+    #df.columns = columns
+    #df['Datum'] = pd.to_datetime(df['Datum'])
 
     # calculate prct
-    df['TotalCH_prct'] = round(df['TotalCH']/df['TotalCH_max']*100, 1)
+    #df['TotalCH_prct'] = round(df['TotalCH']/df['TotalCH_max']*100, 1)
 
     # waterfall chart
-    df_wf = df[['Datum', 'TotalCH_prct']]
+    #df_wf = df[['Datum', 'TotalCH_prct']]
 
-    df_wf['Jahr'] =  df_wf['Datum'].dt.year
-    df_wf['Woche'] = pd.to_datetime(df_wf.Datum).dt.strftime('-W%W')
-    df_wf['Jahr_Woche'] = '2000'+df_wf['Woche'].astype(str)
+    #df_wf['Jahr'] =  df_wf['Datum'].dt.year
+    #df_wf['Woche'] = pd.to_datetime(df_wf.Datum).dt.strftime('-W%W')
+    #df_wf['Jahr_Woche'] = '2000'+df_wf['Woche'].astype(str)
+
+    
 
     return df_wf
 
-    #waterfall_q_data.to_csv('test_data.csv')
+    
