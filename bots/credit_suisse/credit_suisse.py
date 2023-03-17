f411f00eee85f0181c8d6593c962db8f0ffc4efd
webmaster@simonhuwiler.ch <webmaster@simonhuwiler.ch>
2023-03-17 08:23:47 +0100

    add cs

---
+++ bots/credit_suisse/credit_suisse.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 17:52:11 2023
+
+@author: florianseliger
+"""
+
+
+import yfinance as yf
+from datetime import date, timedelta
+import os
+
+from helpers import *
+
+# Set Working Directory
+os.chdir(os.path.dirname(__file__))
+
+
+# SIX
+#df = pd.read_json('https://www.six-group.com/fqs/charts.json?select=ValorId&where=ValorId=CH0012138530CHF4&netting=2&fromdate=20230316&todate=20230316')
+
+tickers = ["CSGN.SW"]  # Subtitute for the tickers you want
+df_1 = yf.download(tickers,  period = "1y", interval = "1d")
+df_1 = df_1['Close'].to_frame().dropna().reset_index(level = 0)
+
+#update_chart(id='9039ce8be0b7e1650165751c47d993d4',
+ #                data=df_1)
+
+
+date_yesterday = date.today() - timedelta(1)
+
+close = df_1[df_1['Date'] == date_yesterday.strftime('%Y-%m-%d')][['Close']].reset_index()
+
+df_2 = yf.download(tickers,  period = "1d", interval = "1m")
+df_2 = df_2['Close'].to_frame().dropna().reset_index(level = 0)
+df_2 = df_2.rename(columns = {'Datetime': 'Date'})
+
+
+df_2['Date'] = df_2['Date'].astype(str)
+
+df_2['Date'] = df_2['Date'].str.slice(start = 0, stop = 19)
+
+df_2 = df_2.rename(columns = {'Close': 'Aktueller Kurs'})
+
+df_2['Schlusskurs vor einem Tag'] = close.Close
+
+df_2.fillna(method='ffill', inplace=True)
+
+
+notes = 'Die Grafik wird alle 30 Minuten aktualisiert.'
+
+update_chart(id = '0e4679180159fe79f9fd140fe620b4e3', data = df_2, notes = notes)
+
+
+df_3 = yf.download(tickers,  period = "16y", interval = "1d")
+df_3 = df_3['Close'].rolling(7).mean().dropna().reset_index(level = 0)
+
+update_chart(id = '0e4679180159fe79f9fd140fe63e3945', data = df_3)
+
+
+# Bankenvergleich
+tickers = ["CSGN.SW", "BNP.PA", "UBSG.SW", "DBK.DE", "GLE.PA", "UCG.MI"]  # Subtitute for the tickers you want
+df_4 = yf.download(tickers,  period = "1y", interval = "1d")
+
+df_4 = df_4['Close']
+
+for i in tickers:
+    x = df_4[i].head(1)
+    df_4[i] = df_4[i]*100/x.values
+
+
+df_4 = df_4.rename(columns = {'CSGN.SW': 'Credit Suisse', 'BNP.PA': 'BNP Paribas', 'UBSG.SW': 'UBS', 'DBK.DE': 'Deutsche Bank', 'GLE.PA': 'Société Générale', 'UCG.MI': 'UniCredit'})
+
+df_4 = df_4[['Credit Suisse', 'BNP Paribas', 'UBS', 'Deutsche Bank',  'UniCredit']]
+
+df_4 = df_4.reset_index()
+
+update_chart(id = '7b974a9aaf4217d71f83fde19d517265', data = df_4)
+
+
+df_4 = yf.download(tickers,  period = "1mo", interval = "1d")
+
+df_4 = df_4['Close']
+
+for i in tickers:
+    x = df_4[i].head(1)
+    df_4[i] = df_4[i]*100/x.values
+
+
+df_4 = df_4.rename(columns = {'CSGN.SW': 'Credit Suisse', 'BNP.PA': 'BNP Paribas', 'UBSG.SW': 'UBS', 'DBK.DE': 'Deutsche Bank', 'GLE.PA': 'Société Générale', 'UCG.MI': 'UniCredit'})
+
+df_4 = df_4[['Credit Suisse', 'BNP Paribas', 'UBS', 'Deutsche Bank',  'UniCredit']]
+
+df_4 = df_4.reset_index()
+
+update_chart(id = '7b974a9aaf4217d71f83fde19d59687a', data = df_4)
+
+
---
+++ bots/credit_suisse/readme.md
@@ -0,0 +1,3 @@
+Grafiken für die Credit Suisse.
+
+Daten kommen aus Yahoo Finance und werden mit dem Paket yfinance abgegriffen.
