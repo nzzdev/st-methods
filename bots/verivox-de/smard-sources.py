1b4a4e73236933d8b0257a399fa4ad6422f578d9
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2023-06-20 23:02:51 +0200

    Fix dataframe for Datawrapper API

--- bots/verivox-de/smard-sources.py
+++ bots/verivox-de/smard-sources.py
@@ -907,6 +907,7 @@ if __name__ == '__main__':
         dw_id = 'G2Vtz'
 
         # update Datawrapper chart
+        df_trade.reset_index(inplace=True)
         dw_chart = dw.add_data(chart_id=dw_id, data=df_trade)
         dw.update_chart(chart_id=dw_id, title=title)
         date = {'annotate': {
