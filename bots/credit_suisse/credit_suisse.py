b23b90ee2887e29d6ed0f255ea6607aaf5e79319
Florian Seliger <fs.bwlvwl@googlemail.com>
2023-08-21 14:37:10 +0200

    Update credit_suisse.py

--- bots/credit_suisse/credit_suisse.py
+++ bots/credit_suisse/credit_suisse.py
@@ -180,5 +180,14 @@ update_chart(id='1dda540238574eac80e865faa0dc2348', data=smi)
 #update_chart(id='a78c9d9de3230aea314700dc582d873d', data=dax)
 
 
+# Countrx Garden
 
+tickers = ["2007.HK"]  
+df_5 = yf.download(tickers,  period = "1mo", interval = "1d")
+df_5 = df_5['Close'].to_frame().dropna().reset_index(level = 0)
+
+notes = 'Für den laufenden Tag wird der aktuelle Kurs angezeigt (Intraday).'
+
+update_chart(id='bc8cbef1bec81fe4197d1ebeb4d25691',
+                 data=df_1, notes = notes)
 
