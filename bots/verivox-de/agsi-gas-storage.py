0dd52eb2985ba47d7688cab74dfa67f303046732
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2023-10-02 08:59:27 +0200

    Temp fix for wrong trend data

--- bots/verivox-de/agsi-gas-storage.py
+++ bots/verivox-de/agsi-gas-storage.py
@@ -119,18 +119,17 @@ if __name__ == '__main__':
         dfold = pd.read_csv(
             './data/gas-storage-2011-2021.tsv', sep='\t', index_col=None)
 
-        """
         # temporary fix for wrong storage data
         dfnew.set_index('Datum', inplace=True)
-        dfnew.at['2022-10-13', '2022'] = 95.14
+        dfnew.at['2023-09-29', 'Trend'] = 0.03
+        dfnew.at['2023-09-30', 'Trend'] = -0.03
         dfnew.to_csv(f'./data/{todaystr}-gasspeicher.csv',
                      encoding='utf-8', index=True)
         dfnew = dfnew.reset_index(level=0)
-        """
 
         # temporary fix for wrong trend data
-        dftrend.at['2023-09-29', 'Trend'] = -0.30
-        dftrend.at['2023-09-30', 'Trend'] = 0.33
+        dftrend.at['2023-09-29', 'Trend'] = 0.03
+        dftrend.at['2023-09-30', 'Trend'] = -0.03
         dftrend.to_csv(f'./data/{todaystr}-gasspeicher.csv', encoding='utf-8')
 
         # convert date column to datetime
