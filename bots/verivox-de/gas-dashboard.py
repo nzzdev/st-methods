928fcbd909e9759dd48805b8433de1a2ace75915
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2023-12-29 13:51:24 +0100

    Update gas stock

--- bots/verivox-de/gas-dashboard.py
+++ bots/verivox-de/gas-dashboard.py
@@ -417,11 +417,13 @@ if __name__ == '__main__':
         diff_acstock_str = diff_acstock.astype(str).replace('.', ',')
         diff_importsshare_str = diff_importsshare.astype(str).replace('.', ',')
 
+        # {gasstock_time} = date and time for gas stock description
+
         meta_storage = {'indicatorTitle': 'Gasspeicher', 'date': todaystr, 'indicatorSubtitle': f'Füllstand¹; Ziel: min. 40 % am 1. 2.', 'value': diff_storage, 'valueLabel': f'{diff_storage_str} %',
                         'yAxisStart': storage_y, 'yAxisLabels': storage_ytick, 'yAxisLabelDecimals': 0, 'color': '#ce4631', 'trend': trend_storage, 'chartType': 'area'}
         meta_gas = {'indicatorTitle': 'Gaspreis', 'date': todaystr, 'indicatorSubtitle': f'je kWh für Neukunden', 'value': diff_gas, 'valueLabel': f'{diff_gas_str} Cent',
                     'yAxisStart': gas_y, 'yAxisLabels': gas_ytick, 'yAxisLabelDecimals': 0, 'color': '#ce4631', 'trend': trend_gas, 'chartType': 'line'}
-        meta_gasstock = {'indicatorTitle': 'Börsen-Gaspreis', 'date': todaystr, 'indicatorSubtitle': f'je kWh am Terminmarkt, {gasstock_time}', 'value': diff_gasstock, 'valueLabel': f'{diff_gasstock_str} Cent',
+        meta_gasstock = {'indicatorTitle': 'Börsen-Gaspreis', 'date': todaystr, 'indicatorSubtitle': f'je kWh am Terminmarkt', 'value': diff_gasstock, 'valueLabel': f'{diff_gasstock_str} Cent',
                          'yAxisStart': gas_y, 'yAxisLabels': gas_ytick, 'yAxisLabelDecimals': 0, 'color': '#ce4631', 'trend': trend_gasstock, 'chartType': 'line'}
         meta_strom = {'indicatorTitle': 'Strompreis', 'date': todaystr, 'indicatorSubtitle': f'je kWh für Neukunden', 'value': diff_strom, 'valueLabel': f'{diff_strom_str} Cent',
                       'yAxisStart': strom_y, 'yAxisLabels': strom_ytick, 'yAxisLabelDecimals': 0, 'color': '#374e8e', 'trend': trend_strom, 'chartType': 'line'}
