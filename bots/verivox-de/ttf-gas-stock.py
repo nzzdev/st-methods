3e9c933da3dfe4f2c64ae6094949a609938db335
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2024-01-08 22:46:38 +0100

    Use Yahoo finance data only

--- bots/verivox-de/ttf-gas-stock.py
+++ bots/verivox-de/ttf-gas-stock.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
         df.index.rename('Datum', inplace=True)
         df['Kosten'] = df['Kosten'].round(0).astype(int)
 
-        # old historical ICE data from theice.com/products/27996665/Dutch-TTF-Gas-Futures/data?marketId=5396828
         """
+        # START historical ICE data from theice.com/products/27996665/Dutch-TTF-Gas-Futures/data?marketId=5396828
         url = 'https://www.theice.com/marketdata/DelayedMarkets.shtml?getHistoricalChartDataAsJson=&marketId=' + \
             market_id + '&historicalSpan=3'
         resp = download_data(url, headers=fheaders)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
         # round numbers
         df['Kosten'] = df['Kosten'].round(0).astype(int)
-        """
+        # END  old historical ICE data
 
         # get latest intraday data
         url = 'https://www.theice.com/marketdata/DelayedMarkets.shtml?getIntradayChartDataAsJson=&marketId=' + market_id
@@ -55,6 +55,7 @@ if __name__ == '__main__':
                                 'Datum', 'Intraday'])
         df_intra['Datum'] = pd.to_datetime(df_intra['Datum'])
         df_intra.set_index('Datum', inplace=True)
+        """
 
         # save current price as csv for dashboard
         df_intra_today = df.copy()
@@ -64,9 +65,10 @@ if __name__ == '__main__':
         df_intra_today = pd.concat(
             [df_intra_today.tail(2)])
         df_intra_today.to_csv('./data/ttf-gas-stock-dash.csv')
+        print(df_intra_today)
 
-        # HOURLY PRICES (not reliable)
         """
+        # START hourly prices (not reliable)
         # save current price as csv for dashboard
         df_intra_today = df_intra.copy()
         df_intra_today = df.copy()
@@ -77,7 +79,7 @@ if __name__ == '__main__':
             [df_intra_today.head(1), df_intra_today.tail(1)])
             [df_intra_today.tail(2)])
         df_intra_today.to_csv('./data/ttf-gas-stock-dash.csv')
-        """
+        # END hourly prices (not reliable)
 
         # calculate intraday mean and drop everything except last row
         df_intra['Kosten'] = df_intra['Intraday'].mean()
@@ -89,22 +91,24 @@ if __name__ == '__main__':
         # drop last pseudo historical value from Yahoo and replace with intraday data
         df.drop(df.tail(1).index, inplace=True)
         df_full = pd.concat([df, df_intra])
+        """
 
         # dynamic chart title
-        title_mwh = df_full[df_full.columns[0]].iloc[-1].round(0).astype(int)
+        title_mwh = df[df.columns[0]
+                       ].iloc[-1].round(0).astype(int)  # old: df_full
         title = f'Gas kostet an der Börse im Schnitt {title_mwh} Euro je MWh'
 
         # create date for chart notes
-        timecode = df_full.index[-1]
+        timecode = df.index[-1]  # old: df_full
         timecode_str = timecode.strftime('%-d. %-m. %Y')
-        notes_chart = '¹ Preise für Terminkontrakte mit Lieferung im nächsten Monat; aktueller Tag: Tagesdurchschnitt.<br>Stand: ' + timecode_str
+        notes_chart = '¹ Preise für Terminkontrakte mit Lieferung im nächsten Monat.<br>Stand: ' + timecode_str
 
         # convert DatetimeIndex
         #df_full.index = df_full.index.strftime('%Y-%m-%d')
 
         # run Q function
         update_chart(id='4decc4d9f742ceb683fd78fa5937acfd',
-                     title=title, notes=notes_chart, data=df_full)
+                     title=title, notes=notes_chart, data=df)  # old: df_full
 
     except:
         raise
