1820ffe4cef8bd6d517dfac3c38c2ca4cd0c5285
Simon Haas <31181952+somm-doe@users.noreply.github.com>
2024-08-19 19:16:18 +0200

    Fix date error

--- bots/verivox-de/bnetza-exporte.py
+++ bots/verivox-de/bnetza-exporte.py
@@ -28,8 +28,9 @@ if __name__ == '__main__':
         df = df.drop(df.columns[[1, 2, 3, 4, 5, 6, 7, 8]], axis=1)
 
         # convert date string to datetime
-        df[df.columns[0]] = pd.to_datetime(
-            df[df.columns[0]], format='%d.%m.%Y')
+        df[df.columns[0]] = pd.to_datetime(df[df.columns[0]], format='%d.%m.%Y')
+        # old date format
+        #df[df.columns[0]] = pd.to_datetime(df[df.columns[0]], format='%d.%m.%Y')
 
         # set date as index
         df = df.set_index(df.columns[0])
